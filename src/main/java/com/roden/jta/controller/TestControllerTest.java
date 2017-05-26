4f1f585bd3214db1feb1bf230ceefd757884841a
Administrator <Administrator@AGOE2345-311800>
2017-05-26 13:28:39 +0800

    spring+jta多数据源

---
+++ src/main/java/com/roden/jta/controller/TestControllerTest.java
@@ -0,0 +1,14 @@
+package com.roden.jta.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestControllerTest {
+
+	@Test
+	public void testTest() {
+		fail("Not yet implemented");
+	}
+
+}
